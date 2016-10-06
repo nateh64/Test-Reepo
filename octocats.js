a2926f940ebe2bca8a66ebe830d818ecb2ca1445
nateh64 <7nhuzenis@stpatrick.org>
2016-10-06 14:12:21 -0500

    created octocat object

---
+++ octocats.js
@@ -0,0 +1,18 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+
+function Octocat (n, a)
+{
+  this.name=n;
+  this.arms=a;
+}
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
